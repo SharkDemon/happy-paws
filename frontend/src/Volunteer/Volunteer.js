9353cbfac2df33bd82eba58829b5a6763a1beaec
seraphyne <renatafd717@gmail.com>
2020-10-31 02:46:52 -0500

    changed the volunteer fields

--- frontend/src/Volunteer/Volunteer.js
+++ frontend/src/Volunteer/Volunteer.js
@@ -7,26 +7,35 @@ export default class Volunteer extends React.Component {
                 <h1>Volunteer Drivers</h1>
                 <div>
                     <ul>
-                        <li>Upcoming Trips: </li>
-                        <li>Leg of the Trip: </li>
-                        <li>License Plate: </li>
-                        <li>Vehicle Model: </li>
+                        <li>First Name: </li>
+                        <li>Last Name: </li>
+                        <li>Birthday: </li>
+                        <li>City: </li>
+                        <li>State: </li>
+                        <li>Zip Code: </li>
+                        <li>Verified: </li>
                     </ul>
                 </div>
                 <div>
                     <ul>
-                        <li>Upcoming Trips: </li>
-                        <li>Leg of the Trip: </li>
-                        <li>License Plate: </li>
-                        <li>Vehicle Model: </li>
+                        <li>First Name: </li>
+                        <li>Last Name: </li>
+                        <li>Birthday: </li>
+                        <li>City: </li>
+                        <li>State: </li>
+                        <li>Zip Code: </li>
+                        <li>Verified: </li>
                     </ul>
                 </div>
                 <div>
                     <ul>
-                        <li>Upcoming Trips: </li>
-                        <li>Leg of the Trip: </li>
-                        <li>License Plate: </li>
-                        <li>Vehicle Model: </li>
+                        <li>First Name: </li>
+                        <li>Last Name: </li>
+                        <li>Birthday: </li>
+                        <li>City: </li>
+                        <li>State: </li>
+                        <li>Zip Code: </li>
+                        <li>Verified: </li>
                     </ul>
                 </div>
             </div>
