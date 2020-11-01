05581c310adc0ee7a531d708d5b18d290bdcab97
RD <rory.c.dickinson@gmail.com>
2020-11-01 01:45:49 -0500

    CORS support

--- backend/src/main/java/com/rd/hackathon/happypaws/controller/TripsController.java
+++ backend/src/main/java/com/rd/hackathon/happypaws/controller/TripsController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.rd.hackathon.happypaws.service.TripLegService;
 import com.rd.hackathon.happypaws.service.TripService;
 
 @RestController
+@CrossOrigin(origins="*", maxAge=120)
 @RequestMapping("/api/trips")
 public class TripsController {
 
